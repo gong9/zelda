c5cc72255504cfa075573e725ae464c46a029b87
gongzhen <517441540@qq.com>
2023-07-02 01:55:14 +0800

    fix: adjust publish scripts name

--- packages/zelda-publish-lint/src/init.ts
+++ packages/zelda-publish-lint/src/init.ts
@@ -14,7 +14,7 @@ export default () => {
       ...configData,
       scripts: {
         ...(configData.scripts || {}),
-        'lint-and-publish': 'npm run lint-and-publish',
+        'lint-publish': 'npm run lint-and-publish',
       },
     } as Config)
   }
